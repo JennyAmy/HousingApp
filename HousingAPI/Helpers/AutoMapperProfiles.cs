4b347708d789d806bf92c6f0c1d8e88698fd3acf
Jennifer Oliseyenum <jennifer.oliseyenum@fintraksoftware.com>
2022-03-08 02:51:54 -0800

    Initial

---
+++ HousingAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HousingAPI.Dtos;
+using HousingAPI.Dtos.PropertyDto;
+using HousingAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingAPI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<City, CityDto>().ReverseMap();
+
+            CreateMap<City, CityNameUpdateDto>().ReverseMap();
+
+            CreateMap<Property, AddPropertyDto>().ReverseMap();
+
+            CreateMap<Photo, PhotoDto>().ReverseMap();
+
+
+            CreateMap<Property, PropertyListDto>()
+                .ForMember(p => p.City, opt => opt.MapFrom(src => src.City.CityName))
+                .ForMember(p => p.PropertyType, opt => opt.MapFrom(src => src.PropertyType.Name))
+                .ForMember(p => p.Country, opt => opt.MapFrom(src => src.City.Country))
+                .ForMember(p => p.FurnishingType, opt => opt.MapFrom(src => src.FurnishingType.Name));
+
+
+            CreateMap<Property, PropertyDetailDto>()
+                .ForMember(p => p.City, opt => opt.MapFrom(src => src.City.CityName))
+                .ForMember(p => p.PropertyType, opt => opt.MapFrom(src => src.PropertyType.Name))
+                .ForMember(p => p.Country, opt => opt.MapFrom(src => src.City.Country))
+                .ForMember(p => p.FurnishingType, opt => opt.MapFrom(src => src.FurnishingType.Name));
+
+            CreateMap<PropertyType, PropertyTypeDto>().ReverseMap();
+
+            CreateMap<FurnishingType, FurnishingTypeDto>().ReverseMap();
+        }
+    }
+}
